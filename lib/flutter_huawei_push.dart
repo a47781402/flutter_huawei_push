4544ac0c5dc30c197bb24a9084ab1194bda924c0
zhuhaixuan <123456>
2020-05-12 02:12:53 +0800

    getToken3

--- lib/flutter_huawei_push.dart
+++ lib/flutter_huawei_push.dart
@@ -11,21 +11,28 @@ class FlutterHuaweiPush {
 //  static const MethodChannel _channel =
 //      const MethodChannel('flutter_huawei_push');
   final MethodChannel _channel;
+  final EventChannel _eventChannel;
 
   @visibleForTesting
-  FlutterHuaweiPush.private(MethodChannel channel) : _channel = channel;
+  FlutterHuaweiPush.private(MethodChannel channel,EventChannel eventChannel) : _channel = channel, _eventChannel = eventChannel;
 
   static final FlutterHuaweiPush _instance =
-      new FlutterHuaweiPush.private(const MethodChannel('flutter_huawei_push'));
+      new FlutterHuaweiPush.private(const MethodChannel('flutter_huawei_push'),const EventChannel('flutter_huawei_push_event'));
 
   Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
+
+
   /// change
    Future<String> get getToken async {
-    final String token = await _channel.invokeMethod('getToken');
+      String token ;
+     _channel.invokeMethod('getToken');
+     _eventChannel.receiveBroadcastStream().listen((data){
+        token = data;
+    });
     return token;
   }
 
